b015828d99b043b37e99c95d43cbde6d10870f76
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-11-22 17:22:11 -0300

    Cria testes de about iniciais

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../pages/About';
+
+test('Checa about', () => {
+  renderWithRouter(<About />);
+  const pokedexHead = screen.getByRole('heading', {
+    name: 'About Pokédex',
+  });
+  expect(pokedexHead).toBeInTheDocument();
+
+  const image = screen.getByAltText('Pokédex');
+  expect(image).toBeInTheDocument();
+  expect(image.getAttribute('src')).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
