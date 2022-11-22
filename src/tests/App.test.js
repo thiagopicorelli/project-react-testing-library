5b620049c26e0a5eac6ae74001291f8fae0b61b2
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-11-22 17:03:01 -0300

    Cria testes iniciais para App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '.././App';
+
+test('Checa links de navegação', () => {
+  renderWithRouter(<App />);
+  const linkCheck = (name) => {
+    const link = screen.getByRole('link', {
+      name: name,
+    });
+    expect(link).toBeInTheDocument();
+  };
+  
+  linkCheck('Home');
+  linkCheck('About');
+  linkCheck('Favorite Pokémon');
+});
