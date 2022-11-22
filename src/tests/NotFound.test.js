f2fc585836b7e348a70be4204a437a099983f5ea
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-11-22 17:29:36 -0300

    Cria testes iniciais de NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+test('Checa NotFound', () => {
+  renderWithRouter(<NotFound />);
+  const notFound = screen.getByRole('heading', {
+    name: 'Page requested not found',
+  });
+  expect(notFound).toBeInTheDocument();
+
+  const image = screen.getByAltText('Pikachu crying because the page requested was not found');
+  expect(image).toBeInTheDocument();
+  expect(image.getAttribute('src')).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
