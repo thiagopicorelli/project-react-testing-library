cfc353f187c89fad44a9dfab8c015b2999ec353d
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-11-22 17:26:03 -0300

    Cria testes inicias de FavoritePokemon

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemon from '../pages/FavoritePokemon';
+
+test('Checa FavoritePokemon', () => {
+  renderWithRouter(<FavoritePokemon />);
+  const noPokemonFound = screen.getByText('No favorite Pokémon found');
+  expect(noPokemonFound).toBeInTheDocument();
+});
