4bc44b2b915515e5b0b94cef0703db833e5c95d7
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-11-22 17:03:48 -0300

    Corrige erros do lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
-import App from '.././App';
+import App from '../App';
 
 test('Checa links de navegação', () => {
   renderWithRouter(<App />);
   const linkCheck = (name) => {
     const link = screen.getByRole('link', {
-      name: name,
+      name,
     });
     expect(link).toBeInTheDocument();
   };
-  
   linkCheck('Home');
   linkCheck('About');
   linkCheck('Favorite Pokémon');
