ecab35ccc828945a86fba44f97a84c3d9c44d573
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-11-23 17:12:51 -0300

    Cria teste inicial da página da pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import pokemonList from '../data';
+import Pokedex from '../pages/Pokedex';
+
+test('Checa Pokedex', () => {
+  renderWithRouter(<Pokedex
+    pokemonList={ pokemonList }
+    isPokemonFavoriteById={ {} }
+  />);
+
+  const typeButtons = screen.getAllByTestId('pokemon-type-button');
+  const pokemonTypes = [
+    ...new Set(pokemonList.reduce((types, { type }) => [...types, type], [])),
+  ];
+  typeButtons.forEach((button) => {
+    expect(button).toBeInTheDocument();
+    expect(pokemonTypes).toContain(button.textContent);
+  });
+
+  const allButton = screen.getByRole('button', {
+    name: 'All',
+  });
+  userEvent.click(allButton);
+  expect(allButton).toBeInTheDocument();
+});
