707cfbec29bc14ceb3bdf2486f56222e24aa86d6
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-11-23 18:13:31 -0300

    Cria testes iniciais para pokemondetails

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import pokemonList from '../data';
+import App from '../App';
+
+test('Checa PokemonDetails', () => {
+  const pokemon = pokemonList[0];
+  renderWithRouter(<App />, `/pokemon/${pokemon.id}`);
+
+  const expectHeading = (name) => {
+    const heading = screen.getByRole('heading', {
+      name,
+    });
+    expect(heading).toBeInTheDocument();
+  };
+
+  const expectText = (text) => {
+    const textScreen = screen.getByText(text);
+    expect(textScreen).toBeInTheDocument();
+  };
+
+  expectHeading(`${pokemon.name} Details`);
+  expectHeading(`Game Locations of ${pokemon.name}`);
+  expectHeading('Summary');
+
+  expectText(pokemon.summary);
+  expectText('Pokémon favoritado?');
+
+  const maps = pokemon.foundAt.map((found) => found.map);
+  const images = screen.getAllByAltText(`${pokemon.name} location`);
+  images.forEach((image) => {
+    expect(image).toBeInTheDocument();
+    expect(maps).toContain(image.getAttribute('src'));
+  });
+});
--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -3,8 +3,11 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
-const renderWithRouter = (component) => {
+const renderWithRouter = (component, pathname) => {
   const history = createMemoryHistory();
+  if (pathname !== undefined) {
+    history.push({ pathname });
+  }
   return ({
     ...render(<Router history={ history }>{component}</Router>), history,
   });
